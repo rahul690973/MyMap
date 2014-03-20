252af4ebcb3163dcc0007d9398a9450dacd7b3b5
rahul690973 <rahul.110392@gmail.com>
2014-03-20 23:52:20 +0530

    sc

--- MyMap/app.js
+++ MyMap/app.js
@@ -44,7 +44,7 @@ Ext.application({
     ],
 
     launch: function() {
-        alert("in launch");
+        
 
 
         Ext.create('MyMap.store.GreetingStore', {
@@ -54,12 +54,12 @@ Ext.application({
                     
                     
                     console.log(records);
-                    longitude=12.9590626;
-                    latitude=77.6561126;
+                    longitude=12.9590626; // temp lat and long for showing on map
+                    latitude=77.6561126;  // as OpenMRS returns NULL by default for lat and long
 
                     console.log(latitude+" "+longitude+ "ty");
         // The following is accomplished with the Google Map API
-        var position = new google.maps.LatLng(longitude, latitude),  //Sencha HQ
+        var position = new google.maps.LatLng(longitude, latitude),  
 
 
             infowindow = new google.maps.InfoWindow({
@@ -135,7 +135,7 @@ Ext.application({
 
         var mapdemo = Ext.create('Ext.Map', {
             mapOptions : {
-                center : new google.maps.LatLng(longitude, latitude),  //nearby San Fran
+                center : new google.maps.LatLng(longitude, latitude),  
                 zoom : 12,
                 mapTypeId : google.maps.MapTypeId.ROADMAP,
                 navigationControl: true,
@@ -173,7 +173,7 @@ Ext.application({
                 maprender: function(comp, map) {
                     var marker = new google.maps.Marker({
                         position: position,
-                        title : 'Sencha HQ',
+                        title : 'My Home',
                         map: map
                     });
 
@@ -199,142 +199,6 @@ Ext.application({
                 }
             }
         });
-        // console.log(latitude+" "+longitude+ "ty");
-        // // The following is accomplished with the Google Map API
-        // var position = new google.maps.LatLng(longitude, latitude),  //Sencha HQ
-
-
-        //     infowindow = new google.maps.InfoWindow({
-        //         content: 'Sencha HQ'
-                 
-        //     }),
-
-        //     //Tracking Marker Image
-        //     image = new google.maps.MarkerImage(
-        //         'resources/images/point.png',
-        //         new google.maps.Size(32, 31),
-        //         new google.maps.Point(0, 0),
-        //         new google.maps.Point(16, 31)
-        //     ),
-
-        //     shadow = new google.maps.MarkerImage(
-        //         'resources/images/shadow.png',
-        //         new google.maps.Size(64, 52),
-        //         new google.maps.Point(0, 0),
-        //         new google.maps.Point(-5, 42)
-        //     ),
-
-        //     trackingButton = Ext.create('Ext.Button', {
-        //         iconCls: 'locate'
-        //     }),
-
-        //     trafficButton = Ext.create('Ext.Button', {
-        //         pressed: true,
-        //         iconCls: 'maps'
-        //     }),
-
-        //     toolbar = Ext.create('Ext.Toolbar', {
-        //         docked: 'top',
-        //         ui: 'light',
-        //         items: [
-        //             {
-        //                 iconCls: 'home',
-        //                 handler: function() {
-        //                     alert("handler");
-        //                     //disable tracking
-        //                     var segmented = Ext.getCmp('segmented'),
-        //                         pressedButtons = segmented.getPressedButtons(),
-        //                         trafficIndex = pressedButtons.indexOf(trafficButton),
-        //                         newPressed = (trafficIndex != -1) ? [trafficButton] : [];
-        //                     segmented.setPressedButtons(newPressed);
-
-        //                     mapdemo.getMap().panTo(position);
-        //                 }
-        //             },
-        //             {
-        //                 id: 'segmented',
-        //                 xtype: 'segmentedbutton',
-        //                 allowMultiple: true,
-        //                 listeners: {
-        //                     toggle: function(buttons, button, active) {
-        //                         if (button == trafficButton) {
-        //                             mapdemo.getPlugins()[1].setHidden(!active);
-        //                         }
-        //                         else if (button == trackingButton) {
-        //                             var tracker = mapdemo.getPlugins()[0],
-        //                                 marker = tracker.getMarker();
-        //                             marker.setVisible(active);
-        //                             tracker.setTrackSuspended(!active);
-        //                         }
-        //                     }
-        //                 },
-        //                 items: [
-        //                     trackingButton, trafficButton
-        //                 ]
-        //             }
-        //         ]
-        //     });
-
-        // var mapdemo = Ext.create('Ext.Map', {
-        //     mapOptions : {
-        //         center : new google.maps.LatLng(longitude, latitude),  //nearby San Fran
-        //         zoom : 12,
-        //         mapTypeId : google.maps.MapTypeId.ROADMAP,
-        //         navigationControl: true,
-        //         navigationControlOptions: {
-        //             style: google.maps.NavigationControlStyle.DEFAULT
-        //         }
-        //     },
-
-        //     plugins : [
-        //         new Ext.plugin.google.Tracker({
-        //             trackSuspended: true,   //suspend tracking initially
-        //             allowHighAccuracy: false,
-        //             marker: new google.maps.Marker({
-        //                 position: position,
-        //                 title: 'My Current Location',
-        //                 shadow: shadow,
-        //                 icon: image
-        //             })
-        //         }),
-        //         new Ext.plugin.google.Traffic()
-        //     ],
-        //     mapListeners: {
-        //         dragstart: function() {
-        //             var segmented = Ext.getCmp('segmented'),
-        //                 pressedButtons = segmented.getPressedButtons().slice(),
-        //                 trackingIndex = pressedButtons.indexOf(trackingButton);
-        //             if (trackingIndex != -1) {
-        //                 pressedButtons.splice(trackingIndex, 1);
-        //                 segmented.setPressedButtons(pressedButtons);
-        //             }
-        //         }
-        //     },
-
-        //     listeners: {
-        //         maprender: function(comp, map) {
-        //             var marker = new google.maps.Marker({
-        //                 position: position,
-        //                 title : 'Sencha HQ',
-        //                 map: map
-        //             });
-
-        //             google.maps.event.addListener(marker, 'click', function() {
-        //                 infowindow.open(map, marker);
-        //             });
-
-        //             setTimeout(function() {
-        //                 map.panTo(position);
-        //             }, 1000);
-        //         }
-
-        //     }
-        // });
-
-        // Ext.create('Ext.Panel', {
-        //     fullscreen: true,
-        //     layout: 'fit',
-        //     items: [toolbar, mapdemo]
-        // });
+        
     }
 });
